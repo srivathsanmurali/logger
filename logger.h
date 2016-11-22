7a6d53ccdc37e2ac5e9608657671faeccd677c37
Srivathsan Murali <srivathsan92@gmail.com>
2016-11-22 14:21:46 +0100

    added throwing runtime exceptions

--- logger.h
+++ logger.h
@@ -4,7 +4,9 @@
 #include <sstream>
 #include <cstdio>
 #include <cstring>
+#include <stdexcept>
 
+// strips filepath to get filename
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 enum TLoggerLevel {ERROR, WARNING, INFO, DEBUG, DEBUG1, DEBUG2, DEBUG3};
@@ -16,6 +18,7 @@ static const char* TLoggerLevelString[] =
 #define REPORTING_LEVEL INFO
 #endif
 
+// Loggin macros to be used for logging
 #define LOG(LEVEL) \
   if (LEVEL <= REPORTING_LEVEL) \
     Log().get(LEVEL) << TLoggerLevelString[LEVEL] << " " << __TIME__ \
@@ -23,6 +26,7 @@ static const char* TLoggerLevelString[] =
 
 #define LOG_IF(LEVEL, COND) if (COND) LOG(LEVEL)
 
+// Logging engine class uses loggin level to log to stderr
 class Log {
   private:
     TLoggerLevel messageLevel;
@@ -43,4 +47,11 @@ class Log {
       }
     }
 };
+
+// throws runtime error with context details.
+#define throwRuntimeError(x) \
+  std::stringstream ss; \
+  ss << "[" << __TIME__ << ":" << __FILENAME__ << ":" << __LINE__ << "] " \
+      << x; \
+  throw std::runtime_error(ss.str());
 #endif // _LOGGER_H_
