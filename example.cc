0e3b1de169620b51e6a8e07c878c8ec937e3db12
Srivathsan Murali <srivathsan92@gmail.com>
2017-06-13 17:17:15 +0200

    Color ouput added

--- example.cc
+++ example.cc
@@ -3,7 +3,7 @@
 
 int main() {
   const size_t cnt = 10;
-  LOG(INFO) << "Counter ahead";
+  LOG_COLOR(INFO,GREEN) << "Counter ahead";
   for (size_t i = 0; i < cnt; i++) {
     LOG_IF(DEBUG, i%2==0) << i;
   }
--- logger.h
+++ logger.h
@@ -13,6 +13,25 @@ enum TLoggerLevel {ERROR, WARNING, INFO, DEBUG, DEBUG1, DEBUG2, DEBUG3};
 static const char* TLoggerLevelString[] =
                           {"ERROR", "WARNING", "INFO", "DEBUG",
                           "DEBUG1", "DEBUG2", "DEBUG3"};
+enum TColorCodes {
+  RESET, WHITE, RED, GREEN, YELLOW, BLUE, MAGENTA,
+  CYAN, GRAY, BOLD_RED, BOLD_GREEN, BOLD_YELLOW };
+
+static const char* TColorCodesString[] = {
+  "\033[0m",      // RESET
+  "\033[1m",      // WHITE
+  "\033[0;31m",   // RED
+  "\033[0;32m",   // GREEN
+  "\033[0;33m",   // YELLOW
+  "\033[0;34m",   // BLUE
+  "\033[0;35m",   // MAGENTA
+  "\033[0;36m",   // CYAN
+  "\033[1;30m",   // GRAY
+  "\033[1;31m",   // BOLD_RED
+  "\033[1;32m",   // BOLD_GREEN
+  "\033[1;33m"    // BOLD_YELLOW
+};
+
 
 #ifndef REPORTING_LEVEL
 #define REPORTING_LEVEL INFO
@@ -24,7 +43,13 @@ static const char* TLoggerLevelString[] =
     Log().get(LEVEL) << TLoggerLevelString[LEVEL] << " " << __TIME__ \
                      << " [" << __FILENAME__ << ":" << __LINE__ << "] "
 
+#define LOG_COLOR(LEVEL, COLOR) \
+  if (LEVEL <= REPORTING_LEVEL) \
+    Log().getColor(LEVEL, COLOR) << TLoggerLevelString[LEVEL] << " " << __TIME__ \
+                     << " [" << __FILENAME__ << ":" << __LINE__ << "] "
+
 #define LOG_IF(LEVEL, COND) if (COND) LOG(LEVEL)
+#define LOG_IF_COLOR(LEVEL, COND, COLOR) if (COND) LOG_COLOR(LEVEL, COLOR)
 
 // Logging engine class uses loggin level to log to stderr
 class Log {
@@ -39,9 +64,16 @@ class Log {
       messageLevel = level;
       return os;
     }
+    std::ostringstream& getColor(
+        TLoggerLevel level = INFO,
+        TColorCodes color = RESET) {
+      messageLevel = level;
+      os << TColorCodesString[color];
+      return os;
+    }
     ~Log() {
       if (messageLevel <= REPORTING_LEVEL) {
-        os << std::endl;
+        os << std::endl << TColorCodesString[RESET];
         fprintf(stderr, "%s", os.str().c_str());
         fflush(stderr);
       }
